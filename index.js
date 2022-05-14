727dcfd4959eb2bd163d4cb7136ce9d6edcf1ff8
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-05-14 22:28:13 +0530

    mod : return type of the server at an endpoint

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+let postResult = {};
+
 app.route("/example")
     .get((req, res) => {
-        res.send("Hello");
+        res.status(200).json(postResult);
     })
     .post((req, res) => {
         console.log(req.body);
+        postResult = req.body;
         res.status(200).json(req.body);
     });
 
