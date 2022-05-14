acedb1c4db9dcc0aac2c2615372be400851d3aa7
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-05-14 22:25:13 +0530

    mod : return type of the server at an endpoint

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.route("/example")
     })
     .post((req, res) => {
         console.log(req.body);
-        res.status(200).send(res.body);
+        res.status(200).json(req.body);
     });
 
 // setting of port for server to listen
